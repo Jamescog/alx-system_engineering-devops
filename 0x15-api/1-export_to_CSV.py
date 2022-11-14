8f07036c47ebfe663e9405f52b86a0a9eea59b4d
Jamescog <jamescog72@gmail.com>
2022-11-14 15:44:21 +0300

    add task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Gather data from API and store it in csv file
+    API: https://jsonplaceholder.typicode.com/
+"""
+
+
+from requests import get
+from sys import argv
+from csv import writer
+
+
+if __name__ == "__main__":
+    params = {"userId": argv[1]}
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user = get(base_url + "users/{}".format(argv[1]))
+    user = user.json()
+    todos = get(base_url + "todos", params=params)
+    todos = todos.json()
+
+    for t in todos:
+        id = '"{}"'.format(argv[1])
+        name = '"{}"'.format(user['name'])
+        comp = '"{}"'.format(t['completed'])
+        title = '"{}"'.format(t['title'])
+        task = [id, name, comp, title]
+        with open("{}.csv".format(argv[1]), 'a') as f:
+            write = writer(f)
+            write.writerow(task)
+            f.close()
