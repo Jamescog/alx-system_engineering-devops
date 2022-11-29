effecc6dc475d604417179ce52f7411562bba4ac
Jamescog <jamescog72@gmail.com>
2022-11-29 14:56:31 +0300

    Get top ten hot titiles for given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+contain defination for top_ten(subreddit) function
+"""
+
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    prints the titles of the first 10 hot posts
+    listed for a given subreddit
+    """
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Linux arm; rv:99.0"}
+    params = {'limit': '10'}
+    response = get(url, headers=headers, params=params)
+    if response.status_code == 404:
+        return None
+    children = response.json().get('data').get('children')
+
+    for child in children:
+        print(child.get('data').get('title'))
